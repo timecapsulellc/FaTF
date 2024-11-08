c36165d0d4d2c762be08679345b9491e220f80f8
timecapsulellc <eonnodeai@gmail.com>
2024-11-08 15:03:09 +0530

    fix: Update build configuration and remove unnecessary files

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  distDir: '.next',
   reactStrictMode: true,
-  basePath: '',
   images: {
     domains: ['ipfs.thirdwebstorage.com'],
     unoptimized: true
@@ -11,16 +9,6 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
-  },
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: '/:path*',
-          destination: '/_next/:path*',
-        },
-      ],
-    }
   }
 };
 
